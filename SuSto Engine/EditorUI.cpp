96ee4dd2a01dacad9911327c19521a6cccadd635
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-09-20 00:47:11 +0200

    Geometry Test

--- SuSto Engine/EditorUI.cpp
+++ SuSto Engine/EditorUI.cpp
@@ -147,26 +147,51 @@ void EditorUI::GeometryMathTest()
 
 	if (ImGui::Button("Sphere - Sphere"))
 	{
+		contact_sphere_sphere = false;
+		contact_sphere_capsules = false;
+		contact_aabb_aabb = false;
+		contact_obb_obb = false;
+		contact_aabb_ray = false;
 		contact_sphere_sphere = !contact_sphere_sphere;
 	}
 
 	if (ImGui::Button("Sphere - Capsule"))
 	{
+		contact_sphere_sphere = false;
+		contact_sphere_capsules = false;
+		contact_aabb_aabb = false;
+		contact_obb_obb = false;
+		contact_aabb_ray = false;
 		contact_sphere_capsules = !contact_sphere_capsules;
 	}
 
 	if (ImGui::Button("AABB - AABB"))
 	{
+		contact_sphere_sphere = false;
+		contact_sphere_capsules = false;
+		contact_aabb_aabb = false;
+		contact_obb_obb = false;
+		contact_aabb_ray = false;
 		contact_aabb_aabb = !contact_aabb_aabb;
 	}
 
 	if (ImGui::Button("OBB - OBB"))
 	{
+		contact_sphere_sphere = false;
+		contact_sphere_capsules = false;
+		contact_aabb_aabb = false;
+		contact_obb_obb = false;
+		contact_aabb_ray = false;
 		contact_obb_obb = !contact_obb_obb;
 	}
 
 	if (ImGui::Button("AABB - Ray"))
 	{
+		contact_sphere_sphere = false;
+		contact_sphere_capsules = false;
+		contact_aabb_aabb = false;
+		contact_obb_obb = false;
+		contact_aabb_ray = false;
 		contact_aabb_ray = !contact_aabb_ray;
 	}
 
@@ -176,7 +201,8 @@ void EditorUI::GeometryMathTest()
 
 	if (contact_sphere_sphere)
 	{
-		// Sphere 1
+		ImGui::Text("contact_sphere_sphere");
+
 		PSphere s1(2);
 		s1.color.Set(255, 0, 0);
 		s1.SetPos(p1.x, p1.y, p1.z);
@@ -184,7 +210,6 @@ void EditorUI::GeometryMathTest()
 
 		Sphere sph1(p1, 2);
 
-		// Sphere 2
 		PSphere s2(2);
 		s2.color.Set(0, 255, 0);
 		s2.SetPos(p2.x, p2.y, p2.z);
@@ -198,7 +223,8 @@ void EditorUI::GeometryMathTest()
 
 	if (contact_sphere_capsules)
 	{
-		// Sphere 1
+		ImGui::Text("contact_sphere_capsules");
+
 		PSphere s1(2);
 		s1.color.Set(255, 0, 0);
 		s1.SetPos(p1.x, p1.y, p1.z);
@@ -206,13 +232,12 @@ void EditorUI::GeometryMathTest()
 
 		Sphere sph1(p1, 2);
 
-		// Sphere 2
 		PCube s2(2, 2, 2);
 		s2.color.Set(0, 255, 0);
 		s2.SetPos(p2.x, p2.y, p2.z);
 		s2.Render();
 
-		Capsule c(float3(p2.x, p2.y - 2, p2.z), float3(p2.x, p2.y, p2.z), 1.5f);
+		Capsule c(float3(p2.x, p2.y - 1, p2.z), float3(p2.x, p2.y + 1, p2.z), 1.0f);
 
 		if (sph1.Intersects(c))
 			contact = true;
@@ -220,17 +245,70 @@ void EditorUI::GeometryMathTest()
 
 	if (contact_aabb_aabb)
 	{
+		ImGui::Text("contact_aabb_aabb");
 
+		PCube c1(2, 2, 2);
+		c1.color.Set(255, 0, 0);
+		c1.SetPos(p1.x, p1.y, p1.z);
+		c1.Render();
+
+		AABB sph1(float3(p1.x - 1, p1.y - 1, p1.z - 1), float3(p1.x + 1, p1.y + 1, p1.z + 1));
+
+		PCube c2(2, 2, 2);
+		c2.color.Set(0, 255, 0);
+		c2.SetPos(p2.x, p2.y, p2.z);
+		c2.Render();
+
+		AABB sph2(float3(p2.x - 1, p2.y - 1, p2.z - 1), float3(p2.x + 1, p2.y + 1, p2.z + 1));
+
+		if (sph1.Intersects(sph2))
+			contact = true;
 	}
 
 	if (contact_obb_obb)
 	{
+		ImGui::Text("contact_obb_obb");
 
+		PCube c1(2, 2, 2);
+		c1.color.Set(255, 0, 0);
+		c1.SetPos(p1.x, p1.y, p1.z);
+		c1.Render();
+
+		AABB ab1(float3(p1.x - 1, p1.y - 1, p1.z - 1), float3(p1.x + 1, p1.y + 1, p1.z + 1));
+		OBB sph1(ab1);
+
+		PCube c2(2, 2, 2);
+		c2.color.Set(0, 255, 0);
+		c2.SetPos(p2.x, p2.y, p2.z);
+		c2.Render();
+
+		AABB ab2(float3(p2.x - 1, p2.y - 1, p2.z - 1), float3(p2.x + 1, p2.y + 1, p2.z + 1));
+		OBB sph2(ab2);
+
+		if (sph1.Intersects(sph2))
+			contact = true;
 	}
 
 	if (contact_aabb_ray)
 	{
+		ImGui::Text("contact_aabb_ray");
 
+		PCube c1(2, 2, 2);
+		c1.color.Set(255, 0, 0);
+		c1.SetPos(p1.x, p1.y, p1.z);
+		c1.Render();
+
+		AABB sph1(float3(p1.x - 1, p1.y - 1, p1.z - 1), float3(p1.x + 1, p1.y + 1, p1.z + 1));
+
+		PLine l1(p2.x, p2.y, p2.z + 10);
+		l1.color.Set(255, 0, 0);
+		l1.SetPos(p2.x, p2.y, p2.z - 10);
+		l1.Render();
+
+		Ray sph2(float3(p2.x, p2.y, p2.z - 10), float3(p2.x, p2.y, p2.z + 10));
+
+		if (sph1.Intersects(sph2))
+			contact = true;
 	}
 
 	ImGui::End();
