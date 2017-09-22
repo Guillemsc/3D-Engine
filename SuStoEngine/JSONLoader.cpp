103f42b0093e65f2ef90cb08eb6c2b618e42db61
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-09-22 16:09:18 +0200

    Added json open, save and create functions

--- SuStoEngine/JSONLoader.cpp
+++ SuStoEngine/JSONLoader.cpp
@@ -23,6 +23,12 @@ bool JSONLoader::Awake()
 
 	LOG_OUTPUT("%s", name);
 
+	json_object_set_string(ob, "name", "John Smith");
+
+	SaveJSON("package.json");
+
+	CreateJSON("test.json");
+
 	return ret;
 }
 
@@ -62,6 +68,63 @@ JSON_Object* JSONLoader::LoadJSON(const char * path)
 			new_doc.path = path;
 			jsons.push_back(new_doc);
 		}
+	}
+
+	return ret;
+}
+
+void JSONLoader::SaveJSON(const char * path)
+{
+	bool exists = false;
+	for (list<json_doc>::iterator it = jsons.begin(); it != jsons.end(); it++)
+	{
+		if (path == (*it).path)
+		{
+			json_serialize_to_file((*it).value, path);
+			exists = true;
+			break;
+		}
+	}
+
+	if (!exists)
+	{
+		LOG_OUTPUT("Error saving %s, probably wrong file path", path);
+	}
+}
+
+JSON_Object* JSONLoader::CreateJSON(const char * path)
+{
+	JSON_Object* ret = nullptr;
+
+	bool exists = false;
+	for (list<json_doc>::iterator it = jsons.begin(); it != jsons.end(); it++)
+	{
+		if (path == (*it).path)
+		{
+			exists = true;
+			break;
+		}
+	}
+
+	if (exists)
+	{
+		LOG_OUTPUT("Error creating %s. There is already a file with this path/name", path);
+	}
+	else
+	{
+		JSON_Value* root_value = json_value_init_object();
+		JSON_Object* root_object = json_value_get_object(root_value);
+
+		json_doc new_doc;
+
+		new_doc.object = root_object;
+		new_doc.value = root_value;
+		new_doc.path = path;
+		jsons.push_back(new_doc);
+
+		json_serialize_to_file(root_value, path);
+
+		ret = root_object;
 	}
 
 	return ret;
--- SuStoEngine/JSONLoader.h
+++ SuStoEngine/JSONLoader.h
@@ -19,6 +19,8 @@ public:
 
 	bool Awake();
 	JSON_Object* LoadJSON(const char* path);
+	void SaveJSON(const char* path);
+	JSON_Object* CreateJSON(const char* path);
 
 	// Called before quitting
 	bool CleanUp();
