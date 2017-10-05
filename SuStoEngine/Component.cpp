94ad94d7ddb8607bc7d235d33f11667acf4640b8
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-10-05 19:13:50 +0200

    Component base clas

---
+++ SuStoEngine/Component.cpp
@@ -0,0 +1,59 @@
+#include "Component.h"
+
+Component::Component(ComponentType _type, GameObject* _owner)
+{
+	type = _type;
+	owner = _owner;
+
+	enabled = true;
+
+	OnCreate();
+}
+
+Component::~Component()
+{
+	Destroy();
+}
+
+void Component::Enable()
+{
+	if (!enabled)
+	{
+		enabled = true;
+		OnEnable();
+	}
+}
+
+void Component::Disable()
+{
+	if (enabled)
+	{
+		enabled = true;
+		OnDisable();
+	}
+}
+
+bool Component::GetEnabled()
+{
+	return enabled;
+}
+
+void Component::SetEnabled(bool set)
+{
+	set ? Enable() : Disable;
+}
+
+void Component::Destroy()
+{
+	OnDestroy();
+}
+
+GameObject * Component::GetOwner()
+{
+	return owner;
+}
+
+ComponentType Component::GetType()
+{
+	return type);
+}
---
+++ SuStoEngine/Component.h
@@ -0,0 +1,40 @@
+#ifndef __Component_H__
+#define __Component_H__
+
+#include "GeometryMath.h"
+
+class GameObject;
+
+enum ComponentType
+{
+	TRANSFORM
+};
+
+class Component
+{
+public:
+	Component(ComponentType type, GameObject* owner);
+	virtual ~Component();
+
+	GameObject* GetOwner();
+	ComponentType GetType();
+
+	void Enable();
+	void Disable();
+	bool GetEnabled();
+	void SetEnabled(bool set);
+	void Destroy();
+
+private:
+	virtual void OnCreate() {};
+	virtual void OnDestroy() {};
+	virtual void OnEnable() {};
+	virtual void OnDisable() {};
+
+private:
+	bool		  enabled = false;
+	GameObject*   owner = nullptr;
+	ComponentType type;
+};
+
+#endif
