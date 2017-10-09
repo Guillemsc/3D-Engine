73adf8b8cf84725d835dd3889644150a7cb1a896
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2017-10-09 10:56:06 +0200

    crash solved when exiting the inspector

--- SuStoEngine/Inspector.cpp
+++ SuStoEngine/Inspector.cpp
@@ -31,6 +31,33 @@ void Inspector::Draw()
 	if (selected.size() == 1)
 	{
 		// Draw
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("Add Component"))
+			{
+				if (ImGui::MenuItem("Add Transform"))
+					selected[0]->AddComponent(TRANSFORM);
+				if (ImGui::MenuItem("Add Primitive"))
+					selected[0]->AddComponent(PRIMITIVE);
+				ImGui::MenuItem("Add Other1");
+				ImGui::MenuItem("Add Other2");
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenuBar();
+		}
+		// Text Input To Rename the GameObject
+		char name[25];
+		sprintf_s(name, 25, selected[0]->GetName());
+		if (ImGui::InputText("", name, 25, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue))
+			selected[0]->SetName(name);
+
+		ImGui::Separator();
+
+		vector<Component*> components = selected[0]->GetComponents();
+		for (vector<Component*>::iterator it = components.begin(); it != components.end(); ++it)
+		{
+			(*it)->InspectorDraw();
+		}
 	}
 	else if (selected.size() > 1)
 	{
@@ -42,38 +69,7 @@ void Inspector::Draw()
 		// Draw taking in account multiple GameObjects selected !!!!
 	}
 
-	//if (selected_go)
-	//{
-	//	ImGui::BeginMenuBar();
-	//		if (ImGui::BeginMenu("Add Component"))
-	//		{
-	//			if (ImGui::MenuItem("Add Transform"))
-	//				selected_go->AddComponent(TRANSFORM);
-	//			if (ImGui::MenuItem("Add Primitive"))
-	//				selected_go->AddComponent(PRIMITIVE);
-	//			ImGui::MenuItem("Add Other1");
-	//			ImGui::MenuItem("Add Other2");
-	//			ImGui::EndMenu();
-	//		}
-	//	ImGui::EndMenuBar();
-
-	//	// Text Input To Rename the GameObject
-	//	char name[25];
-	//	sprintf_s(name, 25, selected_go->GetName());
-	//	if (ImGui::InputText("", name, 25, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue))
-	//		selected_go->SetName(name);
-
-	//	ImGui::Separator();
-
-	//	list<Component*> components = selected_go->GetComponents();
-	//	for (list<Component*>::iterator it = components.begin(); it != components.end(); ++it)
-	//	{
-	//		(*it)->InspectorDraw();
-	//	}
-	//	
-	//	
-	//	
-	//}
+	
 	
 	igEndDock();
 }
