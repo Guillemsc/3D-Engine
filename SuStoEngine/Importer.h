927a621a0bb3b891bf21374d27a909da5c5ed8d4
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2017-10-19 15:57:01 +0200

    Virtuals added

--- SuStoEngine/Importer.h
+++ SuStoEngine/Importer.h
@@ -1,3 +1,14 @@
 #ifndef _IMPORTER_H_
- 
+#define _IMPORTER_H_
+
+class Importer
+{
+public:
+	virtual bool Import(const char* file, const char* path, std::string& output_file) { return false; };
+	virtual bool Import(const void* buffer, uint size, std::string& output_file) { return false; };
+	virtual bool Load(const char* exported_file, Texture* resource) { return false; };
+	virtual bool LoadCheckers(Texture* resource) { return false; };
+};
+
+
 #endif // !_IMPORTER_H_
